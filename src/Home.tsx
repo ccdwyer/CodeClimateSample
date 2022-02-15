836877f21d085dfb28fa8fa5033c9c93549a728c
Christopher Dwyer <ccdwyer@users.noreply.github.com>
2022-02-15 15:40:54 -0500

    Add Home (#1)

---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Welcome Home.</Text>
+    </View>
+  );
+};
+
+export default Home;
